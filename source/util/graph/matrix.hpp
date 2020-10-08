736227c6ab3cf081aaa2f827b5879fba80cff05b
Mike <mikejasinski7@googlemail.com>
2020-10-08 22:37:11 +0200

    Fix tests directory name

--- source/util/graph/matrix.hpp
+++ source/util/graph/matrix.hpp
@@ -2,6 +2,7 @@
 #define $_MASONC_MATRIX_HPP_$
 
 #include <common.hpp>
+#include <iterator.hpp>
 
 namespace masonc
 {
@@ -9,18 +10,10 @@ namespace masonc
     // https://www5.in.tum.de/pub/heinecke08.pdf
 
     template <typename element_t,
+              template <typename> typename iterator_t = iterator,
               typename size_t = u64>
-    struct matrix
+    struct matrix : public iterable<element_t, iterator_t>
     {
-    private:
-
-    public:
-        struct iterator
-        {
-        private:
-
-        };
-
         matrix(size_t row_count, size_t column_count);
 
         virtual element_t& operator[] (size_t row, size_t column) = 0;
@@ -28,6 +21,59 @@ namespace masonc
 
         virtual size_t row_count() const = 0;
         virtual size_t column_count() const = 0;
+    };
+
+    struct matrix_row_major_iterator_data
+    {
+
+    };
+
+    template <typename element_t>
+    struct matrix_row_major_iterator : public iterator<element_t>
+    {
+
+    };
+
+    template <typename element_t,
+              typename size_t = u64>
+    struct matrix_row_major : public matrix<element_t, matrix_row_major_iterator, size_t>
+    {
+    private:
+        element_t* buffer;
+
+    public:
+        matrix_row_major(size_t row_count, size_t column_count)
+        {
+            buffer = new element_t[row_count * column_count];
+        }
+
+        ~matrix_row_major()
+        {
+            delete[] buffer;
+        }
+
+        // TODO: Implement copy ctor, copy assignment op, move ctor, move assignment op.
+
+        // Matrix overrides
+        element_t& operator[] (size_t row, size_t column) override
+        {
+
+        }
+
+        const element_t& operator[] (size_t row, size_t column) const override
+        { }
+
+        size_t row_count() const override
+        { }
+        size_t column_count() const override
+        { }
+
+        // Iterable overrides
+        matrix_row_major_iterator<element_t> begin() override
+        { }
+
+        matrix_row_major_iterator<element_t> end() override
+        { }
     };
 }
 
